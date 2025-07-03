zongruxie4/mlrun

commit 4241cb8b2e064a3bc6592d76e05162fd3563dd05

[K8S] Configure retries for Kubernetes client using urllib3 (#8220)
### Summary

Configures retries for the Kubernetes Python client using
`urllib3.Retry`, applying it to `CoreV1Api` and `CustomObjectsApi`
clients. This helps reduce flakiness caused by transient connection/read
issues when communicating with the Kubernetes API server.

### Changes

- Introduced a shared `ApiClient` with custom retry logic:
  - Retries `GET` and `HEAD` requests
  - Up to 3 retries for connection and read errors
- Injected this `ApiClient` into `CoreV1Api` and `CustomObjectsApi`
- Passed the same config to `load_incluster_config()` and
`load_kube_config()`

https://iguazio.atlassian.net/browse/ML-9573

server/py/framework/utils/singletons/k8s.py
@@ -21,6 +21,7 @@ import typing
 
 import kubernetes.client.rest as k8s_client_rest
 import kubernetes.dynamic.exceptions as k8s_dynamic_exceptions
+import urllib3
 from kubernetes import client, config
 
 import mlrun
@@ -82,10 +83,26 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
         self.namespace = namespace or mlrun.mlconf.namespace
         self.config_file = mlrun.mlconf.kubernetes.kubeconfig_path or None
         self.running_inside_kubernetes_cluster = False
+        self._api_config = client.Configuration()
+        self._api_config.retries = urllib3.util.Retry(
+            allowed_methods=[
+                "HEAD",
+                "GET",
+            ],
+            read=3,
+            connect=3,
+        )
         try:
+            self._api_client = client.ApiClient(
+                configuration=self._api_config,
+            )
             self._init_k8s_config(log)
-            self.v1api = client.CoreV1Api()
-            self.crdapi = client.CustomObjectsApi()
+            self.v1api = client.CoreV1Api(
+                api_client=self._api_client,
+            )
+            self.crdapi = client.CustomObjectsApi(
+                api_client=self._api_client,
+            )
         except Exception as exc:
             logger.warning(
                 "Cannot initialize kubernetes client", exc=mlrun.errors.err_to_str(exc)
@@ -985,13 +1002,18 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
 
     def _init_k8s_config(self, log=True):
         try:
-            config.load_incluster_config()
+            config.load_incluster_config(
+                client_configuration=self._api_config,
+            )
             self.running_inside_kubernetes_cluster = True
             if log:
                 logger.info("Using in-cluster config.")
         except Exception:
             try:
-                config.load_kube_config(self.config_file)
+                config.load_kube_config(
+                    config_file=self.config_file,
+                    client_configuration=self._api_config,
+                )
                 self.running_inside_kubernetes_cluster = True
                 if log:
                     logger.info("Using local kubernetes config.")

server/py/framework/tests/unit/k8s/__init__.py
@@ -1,0 +1,13 @@
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.

server/py/framework/tests/unit/k8s/test_k8s.py
@@ -1,0 +1,128 @@
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import io
+from dataclasses import dataclass
+from typing import Optional
+
+import pytest
+from urllib3._collections import HTTPHeaderDict
+from urllib3.connectionpool import HTTPConnectionPool, HTTPSConnectionPool
+from urllib3.exceptions import ConnectTimeoutError, ReadTimeoutError
+
+from framework.utils.singletons.k8s import K8sHelper
+
+
+@pytest.fixture(autouse=True)
+def patch_kube_config(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "framework.utils.singletons.k8s.config.load_incluster_config",
+        lambda *_, **__: None,
+    )
+    monkeypatch.setattr(
+        "framework.utils.singletons.k8s.config.load_kube_config",
+        lambda *_, **__: None,
+    )
+
+
+class MockOKResponse:
+    msg = HTTPHeaderDict({"Content-Type": "application/json"})
+    status = 200
+    reason = "OK"
+    version = 11
+
+    def __init__(self) -> None:
+        self._fp = io.BytesIO(b'{"all" : "good"}')
+
+    def read(self, _: Optional[int] = None) -> bytes:
+        return self._fp.read()
+
+    def isclosed(self) -> bool:
+        return False
+
+    def getheader(
+        self,
+        name: str,
+        default: Optional[str] = None,
+    ):
+        return self.msg.get(name, default)
+
+    def getheaders(self):
+        return list(self.msg.items())
+
+
+@dataclass
+class AttemptCounter:
+    count: int = 0
+
+    def inc(self) -> int:
+        self.count += 1
+        return self.count
+
+
+def test_retry_config_values() -> None:
+    retries = K8sHelper()._api_config.retries
+    assert (retries.connect, retries.read) == (3, 3)
+    assert {"GET", "HEAD"} <= set(retries.allowed_methods)
+
+
+@pytest.mark.parametrize(
+    "exc_cls",
+    [
+        ConnectTimeoutError,
+        ReadTimeoutError,
+    ],
+)
+def test_k8s_helper_retries_on_errors(
+    monkeypatch: pytest.MonkeyPatch,
+    exc_cls: type[Exception],
+) -> None:
+    helper = K8sHelper()
+    attempts = AttemptCounter()
+
+    def patched_make_request(
+        self,
+        conn,
+        method,
+        url,
+        *a,
+        **kw,
+    ):
+        if attempts.inc() == 1:
+            raise exc_cls(None, url, "request failed")
+        return MockOKResponse()
+
+    monkeypatch.setattr(
+        HTTPConnectionPool,
+        "_make_request",
+        patched_make_request,
+        raising=True,
+    )
+    monkeypatch.setattr(
+        HTTPSConnectionPool,
+        "_make_request",
+        patched_make_request,
+        raising=True,
+    )
+
+    resp = helper._api_client.call_api(
+        "/api/v1/namespaces",
+        "GET",
+        response_type="str",
+        auth_settings=[],
+        _preload_content=False,
+        _return_http_data_only=True,
+    )
+
+    assert attempts.count == 2
+    assert resp.data == b'{"all" : "good"}'

commit fe4d0738f6922b57a4bcb9a3a82804972eec8aa1

[Bugfix] MLRun K8S client raises an SSL error (#8237)
## Fix SSL-retry regression from
[#8220](https://github.com/mlrun/mlrun/pull/8220)

* **Dev deps**
  * add `testcontainers[k3s]`

* **`K8sHelper`**
  * support providing `kube_config_path`

* **Integration tests** (K3s)
* invalid CA → expect `ApiException` with `urllib3.exceptions.SSLError`
  * valid CA → `list_pods()` works
  
  https://iguazio.atlassian.net/browse/ML-10374

Makefile
@@ -580,6 +580,7 @@ test: clean ## Run mlrun tests
 	set -e ; \
 	COMMON_IGNORE_TEST_FLAGS=$$(echo "\
 	--ignore=tests/integration \
+	--ignore=server/py/services/api/tests/integration \
 	--ignore=tests/system \
 	--ignore=tests/rundb/test_httpdb.py \
 	--ignore=server/py/services/api/migrations \
@@ -629,6 +630,7 @@ test-integration-dockerized: build-test ## Run mlrun integration tests in docker
 		-v /var/run/docker.sock:/var/run/docker.sock \
 		-v $$COVERAGE_MOUNT_PATH:/mlrun/tests/coverage_reports \
 		-e RUN_COVERAGE=$(RUN_COVERAGE) \
+		--add-host=host.docker.internal:host-gateway \
 		$(MLRUN_TEST_IMAGE_NAME_TAGGED) make test-integration
 
 .PHONY: test-integration
@@ -644,6 +646,7 @@ test-integration: clean ## Run mlrun integration tests
 		--durations=100 \
 		-rf \
 		tests/integration \
+		server/py/services/api/tests/integration \
 		tests/rundb/test_httpdb.py && \
 	$(PRINT_COVERAGE_REPORT);
 

server/py/framework/utils/singletons/k8s.py
@@ -21,6 +21,7 @@ import typing
 
 import kubernetes.client.rest as k8s_client_rest
 import kubernetes.dynamic.exceptions as k8s_dynamic_exceptions
+import urllib3
 from kubernetes import client, config
 
 import mlrun
@@ -78,14 +79,33 @@ class SecretTypes:
 
 
 class K8sHelper(mlsecrets.SecretProviderInterface):
-    def __init__(self, namespace=None, silent=False, log=True):
+    def __init__(
+        self,
+        namespace=None,
+        silent=False,
+        log=True,
+        kube_config_path: typing.Optional[str] = None,
+    ):
         self.namespace = namespace or mlrun.mlconf.namespace
-        self.config_file = mlrun.mlconf.kubernetes.kubeconfig_path or None
+        self.config_file = (
+            mlrun.mlconf.kubernetes.kubeconfig_path or kube_config_path or None
+        )
         self.running_inside_kubernetes_cluster = False
+        self._create_clients(log, silent)
+
+    def _create_clients(
+        self,
+        log: bool,
+        silent: bool,
+    ):
         try:
-            self._init_k8s_config(log)
-            self.v1api = client.CoreV1Api()
-            self.crdapi = client.CustomObjectsApi()
+            self._api_config = self._init_k8s_config(log)
+
+            self._api_config.retries = urllib3.util.Retry(read=3, connect=3)
+
+            self._api_client = client.ApiClient(self._api_config)
+            self.v1api = client.CoreV1Api(api_client=self._api_client)
+            self.crdapi = client.CustomObjectsApi(api_client=self._api_client)
         except Exception as exc:
             logger.warning(
                 "Cannot initialize kubernetes client", exc=mlrun.errors.err_to_str(exc)
@@ -93,14 +113,22 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
             if not silent:
                 raise
 
-    def resolve_namespace(self, namespace=None):
+    def resolve_namespace(
+        self,
+        namespace=None,
+    ):
         return namespace or self.namespace
 
     def is_running_inside_kubernetes_cluster(self):
         return self.running_inside_kubernetes_cluster
 
     @raise_for_status_code
-    def list_pods(self, namespace=None, selector="", states=None):
+    def list_pods(
+        self,
+        namespace=None,
+        selector="",
+        states=None,
+    ):
         resp = self.v1api.list_namespaced_pod(
             self.resolve_namespace(namespace), label_selector=selector
         )
@@ -214,7 +242,12 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
             if not _continue:
                 break
 
-    def create_pod(self, pod, max_retry=3, retry_interval=3):
+    def create_pod(
+        self,
+        pod,
+        max_retry=3,
+        retry_interval=3,
+    ):
         if "pod" in dir(pod):
             pod = pod.pod
         pod.metadata.namespace = self.resolve_namespace(pod.metadata.namespace)
@@ -256,7 +289,12 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
                 logger.info("Pod created", pod_name=resp.metadata.name)
                 return resp.metadata.name, resp.metadata.namespace
 
-    def delete_pod(self, name, namespace=None, grace_period_seconds=None):
+    def delete_pod(
+        self,
+        name,
+        namespace=None,
+        grace_period_seconds=None,
+    ):
         try:
             api_response = self.v1api.delete_namespaced_pod(
                 name,
@@ -277,7 +315,12 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
                     exc.status, message=mlrun.errors.err_to_str(exc)
                 ) from exc
 
-    def get_pod(self, name, namespace=None, raise_on_not_found=False):
+    def get_pod(
+        self,
+        name,
+        namespace=None,
+        raise_on_not_found=False,
+    ):
         try:
             api_response = self.v1api.read_namespaced_pod(
                 name=name, namespace=self.resolve_namespace(namespace)
@@ -296,12 +339,20 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
                     raise mlrun.errors.MLRunNotFoundError(f"Pod not found: {name}")
             return None
 
-    def get_pod_phase(self, name, namespace=None):
+    def get_pod_phase(
+        self,
+        name,
+        namespace=None,
+    ):
         return self._get_pod_status(
             name, namespace, raise_on_not_found=True
         ).status.phase.lower()
 
-    def get_pod_status(self, name, namespace=None) -> client.V1PodStatus:
+    def get_pod_status(
+        self,
+        name,
+        namespace=None,
+    ) -> client.V1PodStatus:
         return self._get_pod_status(name, namespace, raise_on_not_found=True).status
 
     def delete_crd(
@@ -389,7 +440,10 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
         return {p.metadata.name: p.status.phase for p in pods}
 
     def get_project_vault_secret_name(
-        self, project, service_account_name, namespace=""
+        self,
+        project,
+        service_account_name,
+        namespace="",
     ):
         namespace = self.resolve_namespace(namespace)
 
@@ -429,7 +483,10 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
         )
 
     def store_project_secrets(
-        self, project, secrets, namespace=""
+        self,
+        project,
+        secrets,
+        namespace="",
     ) -> (str, typing.Optional[mlrun.common.schemas.SecretEventActions]):
         secret_name = self.get_project_secret_name(project)
         action = self.store_secrets_with_retry(secret_name, secrets, namespace)
@@ -472,7 +529,10 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
         return username, access_key
 
     def store_auth_secret(
-        self, username: str, access_key: str, namespace=""
+        self,
+        username: str,
+        access_key: str,
+        namespace="",
     ) -> (str, typing.Optional[mlrun.common.schemas.SecretEventActions]):
         """
         Store the given access key as a secret in the cluster. The secret name is generated from the access key
@@ -580,7 +640,10 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
         return mlrun.common.schemas.SecretEventActions.updated
 
     def read_secret(
-        self, secret_name: str, namespace: str = "", silent=False
+        self,
+        secret_name: str,
+        namespace: str = "",
+        silent=False,
     ) -> typing.Optional[client.V1Secret]:
         namespace = self.resolve_namespace(namespace)
         if not silent:
@@ -602,7 +665,11 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
         return k8s_secret
 
     def read_secret_data(
-        self, secret_name: str, namespace: str = "", load_as_json=False, silent=False
+        self,
+        secret_name: str,
+        namespace: str = "",
+        load_as_json=False,
+        silent=False,
     ) -> typing.Optional[dict[str, str]]:
         k8s_secret = self.read_secret(secret_name, namespace, silent)
         if k8s_secret is None:
@@ -676,7 +743,10 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
 
     @raise_for_status_code
     def delete_secrets(
-        self, secret_name, secrets, namespace=""
+        self,
+        secret_name,
+        secrets,
+        namespace="",
     ) -> typing.Optional[mlrun.common.schemas.SecretEventActions]:
         """
         Delete secrets from a kubernetes secret object
@@ -800,7 +870,11 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
         return configmap_name
 
     @raise_for_status_code
-    def get_configmap(self, name: str, namespace: str = ""):
+    def get_configmap(
+        self,
+        name: str,
+        namespace: str = "",
+    ):
         namespace = self.resolve_namespace(namespace)
         label_name = mlrun_constants.MLRunInternalLabels.resource_name
         configmaps_with_label = self.v1api.list_namespaced_config_map(
@@ -814,7 +888,12 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
         return configmaps_with_label.items[0] if configmaps_with_label.items else None
 
     @raise_for_status_code
-    def delete_configmap(self, name: str, namespace: str = "", raise_on_error=True):
+    def delete_configmap(
+        self,
+        name: str,
+        namespace: str = "",
+        raise_on_error=True,
+    ):
         namespace = self.resolve_namespace(namespace)
 
         try:
@@ -913,14 +992,22 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
         )
 
     @raise_for_status_code
-    def _list_events(self, namespace=None, field_selector=""):
+    def _list_events(
+        self,
+        namespace=None,
+        field_selector="",
+    ):
         resp = self.v1api.list_namespaced_event(
             self.resolve_namespace(namespace), field_selector=field_selector
         )
         return resp.items
 
     @staticmethod
-    def _decode_secret_data(secrets_data, secret_keys=None, load_as_json=False):
+    def _decode_secret_data(
+        secrets_data,
+        secret_keys=None,
+        load_as_json=False,
+    ):
         results = {}
         if not secrets_data:
             return results
@@ -983,7 +1070,7 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
             random.choice(string.ascii_lowercase + string.digits) for _ in range(length)
         )
 
-    def _init_k8s_config(self, log=True):
+    def _init_k8s_config(self, log: bool = True) -> client.Configuration:
         try:
             config.load_incluster_config()
             self.running_inside_kubernetes_cluster = True
@@ -991,16 +1078,19 @@ class K8sHelper(mlsecrets.SecretProviderInterface):
                 logger.info("Using in-cluster config.")
         except Exception:
             try:
-                config.load_kube_config(self.config_file)
+                config.load_kube_config(
+                    config_file=self.config_file,
+                )
                 self.running_inside_kubernetes_cluster = True
                 if log:
                     logger.info("Using local kubernetes config.")
-            except Exception:
+            except Exception as exc:
                 raise RuntimeError(
-                    "Cannot find local kubernetes config file,"
-                    " place it in ~/.kube/config or specify it in "
-                    "KUBECONFIG env var"
-                )
+                    "Cannot find local kubernetes config file, "
+                    "place it in ~/.kube/config or specify it in KUBECONFIG env var"
+                ) from exc
+
+        return client.Configuration.get_default_copy()
 
     @staticmethod
     def _hash_access_key(access_key: str):

server/py/services/api/tests/integration/__init__.py
@@ -1,0 +1,13 @@
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.

server/py/services/api/tests/integration/k8s/__init__.py
@@ -1,0 +1,13 @@
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.

server/py/services/api/tests/integration/k8s/conftest.py
@@ -1,0 +1,96 @@
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import base64
+import logging
+import os
+import pathlib
+import typing
+
+import pytest
+import yaml
+
+import framework.utils.singletons.k8s
+
+if typing.TYPE_CHECKING:
+    import testcontainers.k3s
+
+
+@pytest.fixture(scope="session")
+def k3s():
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("testcontainers").setLevel(logging.DEBUG)
+
+    # Ensure that the testcontainers library can find the Docker socket when running inside a Docker container.
+    # This env var is parsed by testcontainers at import time.
+    os.environ["TESTCONTAINERS_HOST_OVERRIDE"] = "host.docker.internal"
+    import testcontainers.k3s
+
+    container = testcontainers.k3s.K3SContainer().with_kwargs(
+        privileged=True,
+    )
+    with container:
+        yield container
+
+
+@pytest.fixture
+def raw_kubeconfig(
+    k3s: "testcontainers.k3s.K3SContainer",
+) -> dict:
+    return yaml.safe_load(k3s.config_yaml())
+
+
+@pytest.fixture
+def valid_kubeconfig_path(
+    tmp_path: pathlib.Path,
+    raw_kubeconfig: dict,
+) -> str:
+    path = tmp_path / "kubeconfig.yaml"
+    yaml.safe_dump(raw_kubeconfig, path.open("w"))
+    return str(path)
+
+
+@pytest.fixture
+def bad_ca_kubeconfig_path(
+    tmp_path: pathlib.Path,
+    raw_kubeconfig: dict,
+) -> str:
+    bad = raw_kubeconfig.copy()
+    bad["clusters"][0]["cluster"]["certificate-authority-data"] = base64.b64encode(
+        b"not-a-ca"
+    )
+    path = tmp_path / "kubeconfig-badca.yaml"
+    yaml.safe_dump(bad, path.open("w"))
+    return str(path)
+
+
+def _k8s_helper_from_config(
+    cfg_path: str,
+) -> framework.utils.singletons.k8s.K8sHelper:
+    return framework.utils.singletons.k8s.K8sHelper(
+        kube_config_path=cfg_path,
+        silent=False,
+        log=False,
+    )
+
+
+@pytest.fixture
+def invalid_ssl_ca_k8s_helper(
+    bad_ca_kubeconfig_path: str,
+) -> framework.utils.singletons.k8s.K8sHelper:
+    return _k8s_helper_from_config(bad_ca_kubeconfig_path)
+
+
+@pytest.fixture
+def valid_k8s_helper(valid_kubeconfig_path):
+    return _k8s_helper_from_config(valid_kubeconfig_path)

server/py/services/api/tests/integration/k8s/test_k8s_client.py
@@ -1,0 +1,32 @@
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+import urllib3.exceptions
+
+
+@pytest.mark.integration
+def test_ssl_verification_fails(invalid_ssl_ca_k8s_helper):
+    with pytest.raises(urllib3.exceptions.MaxRetryError) as exc:
+        invalid_ssl_ca_k8s_helper.v1api.get_api_resources()
+
+    inner = exc.value.reason
+    assert isinstance(inner, urllib3.exceptions.SSLError)
+    assert "certificate" in str(inner).lower()
+
+
+@pytest.mark.integration
+def test_ssl_verification_succeeds(valid_k8s_helper):
+    pods = valid_k8s_helper.list_pods()
+    assert isinstance(pods, list)